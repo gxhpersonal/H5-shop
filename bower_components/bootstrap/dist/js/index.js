50212a52345045c11259b2f0815a196b39452f5e
gxhpersonal <991158744@qq.com>
2016-06-03 23:43:48 +0800

    布局基本完成

--- bower_components/bootstrap/dist/js/index.js
+++ bower_components/bootstrap/dist/js/index.js
@@ -1,5 +1,7 @@
 $(function () {
-    var mySwiper = new Swiper ('.swiper-container', {
-      pagination : '.swiper-pagination'
+  var mySwiper = new Swiper ('.swiper-container', {
+        pagination: '.swiper-pagination',
+        autoplay:2000,
+        loop:true,
     })
   });
